1b1e85148246fb1f9c70daffff3fe93ae23c33b9
muffinbaskt <muffinbaskt@gmail.com>
2026-07-30 07:12:04 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5139,7 +5139,7 @@ function JobPicker({
                   </span>
                 )}
               </h1>
-              <p className="text-xs text-slate-500 leading-tight">Select a job ⚡</p>
+              <p className="text-xs text-slate-500 leading-tight">Select a job 🔔</p>
             </div>
           </div>
           <div className="flex items-center gap-2">
